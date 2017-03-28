a90bdbd996de431011e5727e2bd9cef34a89cd04
Jack <ptoast4@gmail.com>
2017-03-27 22:25:13 -0400

    removed pandoc. half-functional patch notes menu

--- blizzard/blizzard.py
+++ blizzard/blizzard.py
@@ -1,38 +1,35 @@
 import os
 import discord
 from discord.ext import commands
-from .utils.chat_formatting import pagify
+from discord.ext.commands import formatter
+from .utils import chat_formatting as cf
 from cogs.utils import checks
 from .utils.dataIO import dataIO
 from __main__ import send_cmd_help
 import re
 import aiohttp
 import asyncio
+import bleach
 
-try:  # check if BeautifulSoup4 is installed
+try:
     from bs4 import BeautifulSoup
     soup_available = True
 except:
     soup_available = False
 
-try:  # check if pypandoc is installed
-    import pypandoc
-    pypandoc_available = True
-except:
-    pypandoc_available = False
+# Special thanks to judge2020 for telling me about this message of getting
+# patch notes: https://github.com/judge2020/BattleNetUpdateChecker
+# Embed menus are modified version of the menu cog written by Awoonar Dust#7332
+# https://github.com/Lunar-Dust/Dusty-Cogs/
 
-# Special thanks to judge2020 for telling me about pandoc, and offering their
-# code as a reference: https://github.com/judge2020/BattleNetUpdateChecker
 
-# This cog requires:
-# BeautifulSoup4 :: https://www.crummy.com/software/BeautifulSoup/bs4/doc/
-# pypandoc :: https://pypi.python.org/pypi/pypandoc
-# Pandoc :: http://pandoc.org/
+class Forbidden():
+    pass
 
 
 class Blizzard:
 
-    """Blizzard Game Data"""
+    """Blizzard Game Utilities"""
 
     def __init__(self, bot):
         self.bot = bot
@@ -49,6 +46,223 @@ class Blizzard:
         self.hots_abbr = 'heroes'
         self.header = {"User-Agent": "flapjackcogs/1.0"}
         self.patch_header = {'User-Agent': 'Battle.net/1.0.8.4217'}
+        self.patch_urls = {
+            "overwatch": "https://playoverwatch.com/en-us/game/patch-notes/pc/"
+        }
+        self.emoji = {
+            0: "0⃣",
+            1: "1⃣",
+            2: "2⃣",
+            3: "3⃣",
+            4: "4⃣",
+            5: "5⃣",
+            6: "6⃣",
+            7: "7⃣",
+            8: "8⃣",
+            9: "9⃣",
+            10: "🔟",
+            "up": "⬆️",
+            "down": "⬇️",
+            "next": "➡",
+            "back": "⬅",
+            "yes": "✅",
+            "no": "❌"
+        }
+        self.expired_embed = discord.Embed(title='This menu has exipred due to inactivity.')
+
+    def perms(self, ctx):
+        user = ctx.message.server.get_member(self.bot.user.id)
+        return ctx.message.channel.permissions_for(user)
+
+    async def _add_reactions(self, message, choices: list, page, emoji, loop=False):
+        pages = [choices[x:x + 10] for x in range(0, len(choices), 10)]
+        if page > len(pages):
+            page = 0
+        if page:
+            await self.bot.add_reaction(message, str(emoji['back']))
+        for idx, i in enumerate(pages[page], 1):
+            await self.bot.add_reaction(message, str(emoji[idx]))
+
+        is_last = (page < len(pages) - 1)
+        if not is_last or (is_last and loop):
+            await self.bot.add_reaction(message, str(emoji['next']))
+        return
+
+    async def menu(self, ctx, _type: int, messages, choices: int = 1, **kwargs):
+        """Creates and manages a new menu
+        Required arguments:
+            Type:
+                1- number menu
+                2- confirmation menu
+                3- info menu (basically menu pagination)
+            Messages:
+                Strings or embeds to use for the menu.
+                Pass as a list for number menu
+        Optional agruments:
+            page (Defaults to 0):
+                The message in messages that will be displayed
+            timeout (Defaults to 15):
+                The number of seconds until the menu automatically expires
+            check (Defaults to default_check):
+                The same check that wait_for_reaction takes
+            is_open (Defaults to False):
+                Whether or not the menu can take input from any user
+            emoji (Decaults to self.emoji):
+                A dictionary containing emoji to use for the menu.
+                If you pass this, use the same naming scheme as self.emoji
+            message (Defaults to None):
+                The discord.Message to edit if present
+            loop (Defaults to False):
+                Whether or not the pages loop to the first page at the end"""
+        result = None
+        if _type == 1:
+            result = await self._number_menu(ctx, messages, choices, **kwargs)
+        if _type == 2:
+            result = await self._confirm_menu(ctx, messages, **kwargs)
+        if _type == 3:
+            result = await self._info_menu(ctx, messages, **kwargs)
+
+        return result
+
+    async def show_menu(self,
+                        ctx,
+                        message,
+                        messages,
+                        page):
+        if message:
+            return await self.bot.edit_message(message, messages[page])
+        else:
+            # This must be the initial post
+            return await self.bot.send_message(ctx.message.channel,
+                                               messages[page])
+
+    async def _number_menu(self, ctx, messages, choices, **kwargs):
+        page = kwargs.get('page', 0)
+        timeout = kwargs.get('timeout', 15)
+        check = kwargs.get('check', default_check)
+        is_open = kwargs.get('is_open', False)
+        emoji = kwargs.get('emoji', self.emoji)
+        message = kwargs.get('message', None)
+        loop = kwargs.get('loop', False)
+
+        await self.show_menu(ctx, message, messages)
+
+        await self._add_reactions(message, choices, page, emoji, loop)
+
+        r = await self.bot.wait_for_reaction(
+            emoji=list(emoji.values()),
+            message=message,
+            user=ctx.message.author,
+            check=check,
+            timeout=timeout)
+        if r is None:
+            return None
+
+        reacts = {v: k for k, v in emoji.items()}
+        react = reacts[r.reaction.emoji]
+
+        if react == "next":
+            page += 1
+        elif react == "back":
+            page -= 1
+        else:
+            return react
+
+        try:
+            await self.bot.remove_reaction(message, emoji[react], r.user)
+        except Forbidden:
+            await self.bot.delete_message(message)
+            message = None
+
+        return await self._number_menu(
+            ctx, message,
+            choices, page=page,
+            timeout=timeout,
+            check=check, is_open=is_open,
+            emoji=emoji, message=message,
+            loop=loop)
+
+    async def _confirm_menu(self, ctx, message, **kwargs):
+        timeout = kwargs.get('timeout', 15)
+        check = kwargs.get('check', default_check)
+        emoji = kwargs.get('emoji', self.emoji)
+
+        await self.bot.add_reaction(message, str(emoji['yes']))
+        await self.bot.add_reaction(message, str(emoji['no']))
+
+        r = await self.bot.wait_for_reaction(
+            message=message,
+            check=check,
+            user=ctx.message.author,
+            timeout=timeout)
+        if r is None:
+            return None
+
+        reacts = {v: k for k, v in emoji.items()}
+        react = reacts[r.reaction.emoji]
+
+        if react == "no":
+            return False
+        else:
+            return True
+
+    async def _info_menu(self, ctx, messages, **kwargs):
+        page = kwargs.get("page", 0)
+        timeout = kwargs.get("timeout", 15)
+        is_open = kwargs.get("is_open", False)
+        check = kwargs.get("check", default_check)
+        emoji = kwargs.get("emoji", self.emoji)
+        message = kwargs.get("message", None)
+        choices = len(messages)
+
+        if message is None:
+            reactions_needed = True
+        else:
+            reactions_needed = False
+
+        message = await self.show_menu(ctx, message, messages, page)
+
+        if reactions_needed:
+            await self.bot.add_reaction(message, str(emoji['back']))
+            await self.bot.add_reaction(message, str(emoji['no']))
+            await self.bot.add_reaction(message, str(emoji['next']))
+
+        r = await self.bot.wait_for_reaction(
+            message=message,
+            user=ctx.message.author,
+            check=default_check,
+            timeout=timeout)
+        if r is None:
+            return [None, message]
+
+        reacts = {v: k for k, v in emoji.items()}
+        react = reacts[r.reaction.emoji]
+
+        if react == "next":
+            page += 1
+        if react == "back":
+            page -= 1
+        if react == "no":
+            return ["no", message]
+
+        if page < 0:
+            page = choices - 1
+
+        if page == choices:
+            page = 0
+
+        if self.perms(ctx).manage_messages:
+            await self.bot.remove_reaction(message, emoji[react], r.user)
+        else:
+            await self.bot.delete_message(message)
+            message = None
+
+        return await self._info_menu(
+            ctx, messages,
+            page=page,
+            timeout=timeout,
+            check=check, is_open=is_open,
+            emoji=emoji, message=message)
 
     @commands.group(name="blizzard", pass_context=True)
     async def blizzard(self, ctx):
@@ -114,7 +328,13 @@ class Blizzard:
                        self.product_url,
                        self.hearthstone_abbr])
 
-        await self.print_patch_notes(url)
+        notes = await self.format_patch_notes(url)
+
+        result = await self.menu(ctx, 3, notes)
+        if result[0] == "no":
+            await self.bot.delete_messages([result[1], ctx.message])
+        else:
+            await self.bot.edit_message(result[1], embed=self.expired_embed)
 
     @commands.group(name="overwatch", pass_context=True)
     async def overwatch(self, ctx):
@@ -227,13 +447,45 @@ class Blizzard:
 
     @overwatch.command(name="notes", pass_context=True)
     async def _notes_overwatch(self, ctx):
-        """Latest Overwatch patch notes"""
         url = ''.join([self.base_url,
                        self.overwatch_abbr,
                        self.product_url,
                        self.overwatch_abbr])
 
-        await self.print_patch_notes(url)
+        notes = await self.format_patch_notes(url)
+
+        result = await self.menu(ctx, 3, notes)
+        if result[0] == "no":
+            await self.bot.delete_messages([result[1], ctx.message])
+        else:
+            await self.bot.edit_message(result[1], embed=self.expired_embed)
+
+    @overwatch.command(name="notes2", pass_context=True)
+    async def _notes2_overwatch(self, ctx):
+        """Recent Overwatch patch notes"""
+        url = self.patch_urls['overwatch']
+        async with aiohttp.get(url, headers=self.header) as response:
+            soup = BeautifulSoup(await response.text(), "html.parser")
+        notes = soup.find_all("div", class_="patch-notes-body")
+        embed_list = []
+        icon_url = 'http://i.imgur.com/YZ4w2ey.png'
+        footer = "Use the reaction controls to navigate through previous patch notes!"
+        for n in notes:
+            author = n.h1.string.upper()
+            url = self.patch_urls['overwatch']
+            embed=discord.Embed(url=url)
+            embed.set_author(name=author, icon_url=icon_url)
+            embed.set_footer(text=footer)
+            embed_list.append(embed)
+
+        result = await self.menu(ctx, 3, embed_list)
+        if result[0] == "no":
+            await self.bot.delete_messages([result[1], ctx.message])
+        else:
+            await self.bot.edit_message(result[1], embed=self.expired_embed)
+
+
+
 
     @commands.group(name="starcraft2", pass_context=True)
     async def starcraft2(self, ctx):
@@ -250,7 +502,13 @@ class Blizzard:
                        self.product_url,
                        self.starcraft2_abbr])
 
-        await self.print_patch_notes(url)
+        notes = await self.format_patch_notes(url)
+
+        result = await self.menu(ctx, 3, notes)
+        if result[0] == "no":
+            await self.bot.delete_messages([result[1], ctx.message])
+        else:
+            await self.bot.edit_message(result[1], embed=self.expired_embed)
 
     @commands.group(name="warcraft", pass_context=True)
     async def warcraft(self, ctx):
@@ -268,7 +526,13 @@ class Blizzard:
                        self.product_url,
                        self.warcraft_abbr])
 
-        await self.print_patch_notes(url)
+        notes = await self.format_patch_notes(url)
+
+        result = await self.menu(ctx, 3, notes)
+        if result[0] == "no":
+            await self.bot.delete_messages([result[1], ctx.message])
+        else:
+            await self.bot.edit_message(result[1], embed=self.expired_embed)
 
     @warcraft.command(name="token", pass_context=True)
     async def _token_warcraft(self, ctx, realm: str='na'):
@@ -297,7 +561,13 @@ class Blizzard:
                        self.product_url,
                        self.diablo_abbr])
 
-        await self.print_patch_notes(url)
+        notes = await self.format_patch_notes(url)
+
+        result = await self.menu(ctx, 3, notes)
+        if result[0] == "no":
+            await self.bot.delete_messages([result[1], ctx.message])
+        else:
+            await self.bot.edit_message(result[1], embed=self.expired_embed)
 
     @diablo3.command(name="stats", pass_context=True)
     async def _stats_diablo3(self, ctx, tag: str=None):
@@ -378,27 +648,61 @@ class Blizzard:
                        self.product_url,
                        self.hots_abbr])
 
-        await self.print_patch_notes(url)
+        notes = await self.format_patch_notes(url)
 
-    async def print_patch_notes(self, url):
-        try:
-            async with aiohttp.get(url, headers=self.patch_header) as response:
-                soup = BeautifulSoup(await response.text(), "html.parser")
+        result = await self.menu(ctx, 3, notes)
+        if result[0] == "no":
+            await self.bot.delete_messages([result[1], ctx.message])
+        else:
+            await self.bot.edit_message(result[1], embed=self.expired_embed)
 
-            html_notes = soup.find('div', {"class": "patch-notes-interior"})
-            text_notes = pypandoc.convert_text(html_notes, 'plain',
-                                               format='html',
-                                               extra_args=['--wrap=none'])
-            # Removal/replacement of odd characters
-            text_notes = text_notes.replace('&nbsp;', ' ')
-            text_notes = text_notes.replace('&apos;', "'")
-            msg_list = pagify(text_notes, delims=["\n"])
-            for msg in msg_list:
-                await self.bot.say(msg)
-                await asyncio.sleep(1)
+    async def format_patch_notes(self, url):
+        tags = ['h1', 'h2', 'p', 'li', 'div']
+        attr = {'div': 'class'}
+        async with aiohttp.get(url, headers=self.patch_header) as response:
+            dirty = await response.text()
+        clean = bleach.clean(dirty, tags=tags, attributes=attr, strip=True)
+        soup = BeautifulSoup(clean, "html.parser")
+        notes = soup.find_all('div', class_="patch-notes-interior")
+        note_list = []
+        for note in notes:
+            # Format each patch note into an array of messages using Paginator
+            # save for later: Asciidoc [] for red, ini [] for blue
+            pager = formatter.Paginator(prefix='```ini\n[...\n', suffix='...]```', max_size=1000)
+            for child in note.children:
+                if child.name == 'h1':
+                    # This is a patch notes title, with date.
+                    pager.add_line(child.get_text() + '\n')
+                elif child.name == 'h2':
+                    # Thid is a patch notes section heading.
+                    pager.add_line(child.get_text() + '\n')
+                elif child.name == 'p':
+                    # This is a plain paragraph of patch notes.
+                    pager.add_line(child.get_text() + '\n')
+                elif child.name == 'li':
+                    # A list is about to follow.
+                    # Need to add a function to recursively walk the list
+                    self.walk_list(child, pager)
+                    pager.add_line('-' + child.get_text() + '\n')
+                else:
+                    # It's something different, treat like a paragraph and hope.
+                    #pager.add_line(child.get_text())
+                    pass
+            note_list.append(pager.pages)
 
-        except:
-            await self.bot.say("I couldn't find any patch notes.")
+
+        return note_list[0]
+        # Can say full notes with:
+        #await self.say_full_notes(note_list[0])
+
+    def walk_list(self, child, pager):
+        pass
+
+
+    async def say_full_notes(self, pages):
+        for page in pages:
+            await self.bot.say(page)
+            await asyncio.sleep(1)
 
     async def print_token(self, url, realm):
 
@@ -425,6 +729,12 @@ class Blizzard:
         except:
             await self.bot.say("Error finding WoW token prices.")
 
+def default_check(reaction, user):
+        if user.bot:
+            return False
+        else:
+            return True
+
 
 def check_folders():
     folder = "data/blizzard"
@@ -441,14 +751,11 @@ def check_files():
 
 
 def setup(bot):
-    if soup_available and pypandoc_available:
+    if soup_available:
         check_folders()
         check_files()
         n = Blizzard(bot)
         bot.add_cog(n)
     else:
-        if not soup_available:
-            error_text += "You need to run `pip install beautifulsoup4`\n"
-        if not pypandoc_available:
-            error_text += "You need to run `pip install pypandoc`\n"
+        error_text = "You need to run `pip install beautifulsoup4`"
         raise RuntimeError(error_text)
