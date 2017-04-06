1ef161bd3d15a230c2d7c0b62596d2b6921ce9e0
Jack <jack@fjack.xyz>
2017-04-05 22:28:48 -0400

    server/channel ignore setting, no more mentions
    [p]watignore server, and
    [p]watignore channel
    will now ignore/unignore the current server/channel
    also, switched back to message.clean_content, so no more @mentions

--- wat/wat.py
+++ wat/wat.py
@@ -1,9 +1,12 @@
+import os
 import re
 
-import discord
 from discord.ext import commands
 
-from .utils.chat_formatting import escape_mass_mentions
+from __main__ import send_cmd_help
+
+from .utils import checks
+from .utils.dataIO import dataIO
 
 
 class Wat:
@@ -12,14 +15,60 @@ class Wat:
 
     def __init__(self, bot):
         self.bot = bot
+        self.settings_path = "data/wat/settings.json"
+        self.settings = dataIO.load_json(self.settings_path)
 
-    async def msg_listener(self, message):
+    @commands.group(name="watignore", pass_context=True, no_pm=True)
+    @checks.admin_or_permissions(manage_server=True)
+    async def watignore(self, ctx):
+        """Change Wat cog ignore settings."""
+
+        if ctx.invoked_subcommand is None:
+            await send_cmd_help(ctx)
+
+    @watignore.command(name="server", pass_context=True, no_pm=True)
+    @checks.admin_or_permissions(manage_server=True)
+    async def _watignore_server(self, ctx):
+        """Ignore/Unignore the current server"""
+
+        server = ctx.message.server
+        if server.id in self.settings['ignore_servers']:
+            self.settings['ignore_servers'].remove(server.id)
+            await self.bot.say("wot? Ok boss, I will no longer "
+                               "ignore this server.")
+        else:
+            self.settings['ignore_servers'].append(server.id)
+            await self.bot.say("what? Fine, I will ignore "
+                               "this server.")
+        dataIO.save_json(self.settings_path, self.settings)
+
+    @watignore.command(name="channel", pass_context=True, no_pm=True)
+    @checks.admin_or_permissions(manage_server=True)
+    async def _watignore_channel(self, ctx):
+        """Ignore/Unignore the current channel"""
+
+        channel = ctx.message.channel
+        if channel.id in self.settings['ignore_channels']:
+            self.settings['ignore_channels'].remove(channel.id)
+            await self.bot.say("wut? Ok, I will no longer "
+                               "ignore this channel.")
+        else:
+            self.settings['ignore_channels'].append(channel.id)
+            await self.bot.say("wat? Alright, I will ignore "
+                               "this channel.")
+        dataIO.save_json(self.settings_path, self.settings)
+
+    async def on_message(self, message):
         if message.author.bot:
             return
         if self.is_command(message):
             return
         content = message.content.lower().split()
         if len(content) != 1:
+            return
+        if message.server.id in self.settings['ignore_servers']:
+            return
+        if message.channel.id in self.settings['ignore_channels']:
             return
 
         pattern = re.compile(r'w+h*[aou]+t+[?!]*', re.IGNORECASE)
@@ -28,7 +77,7 @@ class Wat:
                                                    before=message):
                 author = before.author
                 name = author.display_name
-                content = escape_mass_mentions(before.content)
+                content = before.clean_content
                 if not author.bot\
                         and not self.is_command(before)\
                         and not author == message.author\
@@ -51,7 +100,21 @@ class Wat:
         return False
 
 
+def check_folders():
+    folder = "data/wat"
+    if not os.path.exists(folder):
+        print("Creating {} folder...".format(folder))
+        os.makedirs(folder)
+
+
+def check_files():
+    default = {'ignore_channels': [], 'ignore_servers': []}
+    if not dataIO.is_valid_json("data/wat/settings.json"):
+        print("Creating default wat settings.json...")
+        dataIO.save_json("data/wat/settings.json", default)
+
+
 def setup(bot):
-    n = Wat(bot)
-    bot.add_cog(n)
-    bot.add_listener(n.msg_listener, "on_message")
+    check_folders()
+    check_files()
+    bot.add_cog(Wat(bot))
