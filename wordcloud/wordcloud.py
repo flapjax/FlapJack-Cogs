40e55b35e548dff3af0c9575768eacbc2fe06fa9
Jack <ptoast4@gmail.com>
2017-04-14 16:03:55 -0400

    customizable wordcloud masks

--- wordcloud/wordcloud.py
+++ wordcloud/wordcloud.py
@@ -1,8 +1,10 @@
+import asyncio
 import os
 
 import numpy as np
 from __main__ import send_cmd_help
 from discord.ext import commands
+from discord.ext.commands import formatter
 from PIL import Image
 from wordcloud import WordCloud as WCloud
 from wordcloud import ImageColorGenerator
@@ -18,7 +20,25 @@ class WordCloud:
         self.bot = bot
         self.settings_path = "data/wordcloud/settings.json"
         self.log_folder = "data/wordcloud/logs/"
+        self.mask_folder = "data/wordcloud/masks/"
         self.settings = dataIO.load_json(self.settings_path)
+
+    async def _list_masks(self, channel):
+
+        masks = sorted(os.listdir(self.mask_folder))
+
+        if len(masks) == 0:
+            await self.bot.send_message(channel, "No masks found. Use "
+                                        "`{}wordmask add`to add one."
+                                        .format(ctx.prefix))
+
+        pager = formatter.Paginator(prefix='```', suffix='```', max_size=2000)
+        pager.add_line('Here are the image masks you have installed:')
+        for mask in masks:
+            pager.add_line(mask)
+        for page in pager.pages:
+            await self.bot.send_message(channel, page)
+            asyncio.sleep(1)
 
     @commands.command(name='wordcloud', pass_context=True, no_pm=True)
     async def wordcloud(self, ctx, channel=None):
@@ -46,18 +66,21 @@ class WordCloud:
             await self.bot.say("I couldn't find a log for this channel.")
             return
 
-        #mask = np.array(Image.open("data/wordcloud/red3.png"))
-        #coloring = ImageColorGenerator(mask)
+        mask = None
+        coloring = None
+        mask_file = self.settings.get('mask', None)
+        if mask_file is not None:
+            mask = np.array(Image.open(mask_file))
+            coloring = ImageColorGenerator(mask)
 
-        #wc = WCloud(mask=mask, color_func=coloring, max_words=300)
-        wc = WCloud()
+        wc = WCloud(mask=mask, color_func=coloring)
+        #wc = WCloud()
         wc.generate(text)
-
-        tempfile = 'data/wordcloud/clouds/' + channel.id + '.png'
-        wc.to_file(tempfile)
+        cloudfile = 'data/wordcloud/clouds/' + channel.id + '.png'
+        wc.to_file(cloudfile)
 
         msg = "Word Cloud for **" + server.name + '/' + channel.name + "**:"
-        await self.bot.send_file(ctx.message.channel, tempfile, content=msg)
+        await self.bot.send_file(ctx.message.channel, cloudfile, content=msg)
 
     @commands.group(name='wordlog', pass_context=True, no_pm=True)
     async def wordlog(self, ctx):
@@ -106,6 +129,31 @@ class WordCloud:
                                    "for word clouds in **{}**."
                                    .format(channel.name))
         dataIO.save_json(self.settings_path, self.settings)
+
+    @commands.group(name='wordmask', pass_context=True, no_pm=True)
+    async def wordmask(self, ctx):
+        """WordCloud masking settings"""
+        if ctx.invoked_subcommand is None:
+            await send_cmd_help(ctx)
+            return
+
+    @wordmask.command(name='list', pass_context=True, no_pm=True)
+    async def _wordmask_list(self, ctx):
+        """List image files available for masking"""
+
+        await self._list_masks(ctx.message.channel)
+
+    @wordmask.command(name='set', pass_context=True, no_pm=True)
+    async def _wordmask_set(self, ctx, filename: str):
+        """List image files available for masking"""
+
+        if not os.path.isfile(self.mask_folder + filename):
+            await self.bot.say("That's not a valid filename.")
+            await self._list_masks(ctx.message.channel)
+            return
+        self.settings['mask'] = self.mask_folder + filename
+        dataIO.save_json(self.settings_path, self.settings)
+        await self.bot.say('Mask set to {}.'.format(filename))
 
     async def on_message(self, message):
         # For now, WordCloud will log its own message content, per server
