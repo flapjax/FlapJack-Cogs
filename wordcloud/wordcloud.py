d80547e77332c9fa55e7dd9a85c682bd3b37e7b8
Jack <jack@fjack.xyz>
2017-09-02 16:47:56 -0400

    [wordcloud] remove logging

--- wordcloud/wordcloud.py
+++ wordcloud/wordcloud.py
@@ -19,7 +19,6 @@ class WordCloud:
     def __init__(self, bot):
         self.bot = bot
         self.settings_path = "data/wordcloud/settings.json"
-        self.log_folder = "data/wordcloud/logs/"
         self.mask_folder = "data/wordcloud/masks/"
         self.settings = dataIO.load_json(self.settings_path)
 
@@ -41,7 +40,7 @@ class WordCloud:
             asyncio.sleep(1)
 
     @commands.command(name='wordcloud', pass_context=True, no_pm=True)
-    async def wordcloud(self, ctx, channel=None):
+    async def wordcloud(self, ctx, channel=None, limit: int=1000):
         """Generate a wordcloud"""
 
         if channel is None:
@@ -54,17 +53,11 @@ class WordCloud:
                 return
             server = channel.server
 
-        fp = self.log_folder + channel.id + '.log'
-        # Currently this just grabs the entire log's message content
-        # Will need to add purging and selective word clouds later
         text = ''
-        try:
-            with open(fp, mode='r', encoding='utf-8') as f:
-                for line in f:
-                    text += line[22:]
-        except FileNotFoundError:
-            await self.bot.say("I couldn't find a log for this channel.")
-            return
+        async for message in self.bot.logs_from(channel, limit=limit):
+            if not mssage.author.bot:
+                text += message.clean_content + ' '
+
         # Default settings
         mask = None
         coloring = None
@@ -96,63 +89,8 @@ class WordCloud:
         msg = "Word Cloud for **" + server.name + '/' + channel.name + "**:"
         await self.bot.send_file(ctx.message.channel, cloudfile, content=msg)
 
-    @commands.group(name='wordlog', pass_context=True, no_pm=True)
-    async def wordlog(self, ctx):
-        """WordCloud logging settings"""
-        if ctx.invoked_subcommand is None:
-            await send_cmd_help(ctx)
-            return
-
-    @wordlog.command(name='server', pass_context=True, no_pm=True)
-    async def _wordlog_server(self, ctx, on_off: bool=None):
-        """Turn wordcloud logging on/off for an entire server"""
-        server = ctx.message.server
-        if server.id in self.settings.setdefault('servers', []):
-            if not on_off:  # This also catches None case
-                self.settings['servers'].remove(server.id)
-                await self.bot.say("I will stop logging message content "
-                                   "for word clouds on **{}**. However, "
-                                   "I will still log channels specified by "
-                                   "`{}wordcloud log channel`."
-                                   .format(server.name, ctx.prefix))
-            else:
-                await self.bot.say("I am already logging this server.")
-        else:
-            if on_off or on_off is None:
-                self.settings['servers'].append(server.id)
-                await self.bot.say("I will begin logging message content "
-                                   "for word clouds in all channels I can "
-                                   "see on **{}**.".format(server.name))
-            else:
-                await self.bot.say("I was not logging this server.")
-        dataIO.save_json(self.settings_path, self.settings)
-
-    @wordlog.command(name='channel', pass_context=True, no_pm=True)
-    async def _wordlog_channel(self, ctx, on_off: bool=None):
-        """Turn wordcloud logging on/off for a specific channel"""
-        channel = ctx.message.channel
-        if channel.id in self.settings.setdefault('channels', []):
-            if not on_off:  # This also catches None case
-                self.settings['channels'].remove(channel.id)
-                await self.bot.say("I will stop logging message content "
-                                   "for word clouds in **{}**. However, "
-                                   "I will still log this channel if the "
-                                   "entire server is enabled via "
-                                   "`{}wordcloud log server`."
-                                   .format(channel.name, ctx.prefix))
-            else:
-                await self.bot.say("I am already logging this channel.")
-        else:
-            if on_off or on_off is None:
-                self.settings['channels'].append(channel.id)
-                await self.bot.say("I will begin logging message content "
-                                   "for word clouds in **{}**."
-                                   .format(channel.name))
-            else:
-                await self.bot.say("I am not logging this channel.")
-        dataIO.save_json(self.settings_path, self.settings)
-
     @commands.group(name='wordset', pass_context=True, no_pm=True)
+    @checks.mod_or_permissions(administrator=True)
     async def wordset(self, ctx):
         """WordCloud image settings"""
         if ctx.invoked_subcommand is None:
@@ -238,42 +176,10 @@ class WordCloud:
         dataIO.save_json(self.settings_path, self.settings)
         await self.bot.say("Cleared the excluded word list.")
 
-    async def on_message(self, message):
-        # For now, WordCloud will log its own message content, per server
-        # channel. In the future, something like ActiviyLog or LogItAll
-        # will be used
-        if message.author.bot:
-            return
-        if self.is_command(message):
-            return
-        if message.server is None:
-            return
-        if message.server.id not in self.settings.setdefault('servers', []) and\
-                message.channel.id not in self.settings.setdefault('channels', []):
-            return
-
-        fp = self.log_folder + message.channel.id + '.log'
-        # [YYYY-MM-DD HH:MM:SS]:
-        timestamp = message.timestamp.strftime('[%Y-%m-%d %H:%M:%S]: ')
-        content = timestamp + message.clean_content + '\n'
-        with open(fp, mode='a', encoding='utf-8') as f:
-            f.write(content)
-
-    # Credit to Twentysix26's trigger cog
-    def is_command(self, msg):
-        if callable(self.bot.command_prefix):
-            prefixes = self.bot.command_prefix(self.bot, msg)
-        else:
-            prefixes = self.bot.command_prefix
-        for p in prefixes:
-            if msg.content.startswith(p):
-                return True
-        return False
-
 
 def check_folders():
     root_folder = "data/wordcloud/"
-    folders = ['logs', 'masks', 'clouds']
+    folders = ['masks', 'clouds']
     for folder in folders:
         if not os.path.exists(root_folder + folder):
             print("Creating {} folder...".format(root_folder + folder))
