e0fcb811d4d2e148d50534c5bdad8db45dcdbd03
Unknown <jack@fjack.xyz>
2018-11-26 21:59:32 -0500

    rework spoiler cog to use DMs
    Now that animated gifs play automatically on most clients, the old method of hiding spoilers with a gif is useless. This new spoiler cog delivers the spoiler via DM when the user clicks a reaction.

--- spoiler/__init__.py
+++ spoiler/__init__.py
@@ -1,25 +1,5 @@
 from .spoiler import Spoiler
-import os
-
-try:
-    from PIL import Image, ImageDraw, ImageFont
-    pillowAvailable = True
-except:
-    pillowAvailable = False
-
-
-def check_folders():
-    folder = "cogs/spoiler/data"
-    if not os.path.exists(folder):
-        print("Creating {} folder...".format(folder))
-        os.makedirs(folder)
 
 
 def setup(bot):
-    check_folders()
-    if pillowAvailable:
-        bot.add_cog(Spoiler(bot))
-    else:
-        error_text = ("Make sure Pillow is intalled."
-                      "\n`pip install Pillow`")
-        raise RuntimeError(error_text)
+    bot.add_cog(Spoiler(bot))
--- spoiler/spoiler.py
+++ spoiler/spoiler.py
@@ -1,97 +1,46 @@
-import os
-import random
-import textwrap
-
 import discord
 from redbot.core import commands
-
-try:
-    from PIL import Image, ImageDraw, ImageFont
-    pillowAvailable = True
-except:
-    pillowAvailable = False
 
 BaseCog = getattr(commands, "Cog", object)
 
 
 class Spoiler(BaseCog):
 
-    """Hide spoilers using animated GIFs"""
+    """Hide spoilers by delivering them via DM"""
 
     def __init__(self, bot):
         self.bot = bot
-        self.temp_filepath = "cogs/spoiler/data/"
-        self.line_length = 60
-        self.margin = (5, 5)
-        self.font = "cogs/spoiler/data/UbuntuMono-Regular.ttf"
-        self.font_size = 14
-        self.font_color = 150
-        self.bg_color = 20
+        self.emoji = "👀"
+        self.spoilers = {}
 
-    @commands.command(pass_context=True, no_pm=True)
+    @commands.guild_only()
+    @commands.command(name="spoiler")
     async def spoiler(self, ctx, *, text: str):
-        """Use an animated gif to hide spoiler text"""
-
-        message = ctx.message
-        author = message.author.display_name
-        title = "Mouseover to reveal spoiler"
-        if title == ''.join(text):
-            await ctx.send("Nice try :|")
-            return
+        """Hide spoiler text and have it delivered via DM."""
 
         try:
-            await message.delete()
-        except discord.errors.Forbidden:
+            await ctx.message.delete()
+        except discord.Forbidden:
             await ctx.send("I require the 'manage messages' permission "
                            "to hide spoilers!")
 
-        try:
-            fnt = ImageFont.truetype(self.font, self.font_size)
-        except OSError:
-            await ctx.send("I couldn't load the font file. Try "
-                           "reinstalling via the downloader cog, or "
-                           "manually place `UbuntuMono-Regular.ttf` "
-                           "in `/data/spoiler/`")
-            return
+        content = "**{}** posted a spoiler. React with {} to have it DMed to you.".format(ctx.author.mention, self.emoji)
+        message = await ctx.send(content)
+        await message.add_reaction(self.emoji)
 
-        spoil_lines = []
-        for line in text.splitlines():
-            spoil_lines.extend(textwrap.wrap(line, self.line_length,
-                                             replace_whitespace=False))
+        self.spoilers[message.id] = {
+            'spoiler': text,
+            'deliveries': []  # user ids to which the spoiler has been delivered
+        }
 
-        width = fnt.getsize(title)[0] + 50
-        height = 0
-
-        for line in spoil_lines:
-            size = fnt.getsize(line)
-            width = max(width, size[0])
-            height += size[1] + 2
-
-        width += self.margin[0]*2
-        height += self.margin[1]*2
-
-        spoils = '\n'.join(spoil_lines)
-
-        spoil_img = [self.new_image(width, height) for _ in range(2)]
-        spoil_text = [title, spoils]
-
-        for img, txt in zip(spoil_img, spoil_text):
-            canvas = ImageDraw.Draw(img)
-            try:
-                canvas.text(self.margin, txt, font=fnt, fill=self.font_color,
-                            spacing=4)
-            except TypeError:
-                canvas.text(self.margin, txt, font=fnt, fill=self.font_color)
-
-        path = self.temp_filepath + ''.join(random.choice(
-                   '0123456789ABCDEF') for i in range(12)) + ".gif"
-
-        spoil_img[0].save(path, format="GIF", save_all=True,
-                          append_images=[spoil_img[1]],
-                          duration=[0, 0xFFFF], loop=0)
-        content = "**" + author + "** posted this spoiler:"
-        await message.channel.send(content=content, file=discord.File(path))
-        os.remove(path)
-
-    def new_image(self, width, height):
-        return Image.new("L", (width, height), self.bg_color)
+    async def on_reaction_add(self, reaction, user):
+        msg_id = reaction.message.id
+        if reaction.emoji == self.emoji and msg_id in self.spoilers and user.id != self.bot.user.id:
+            if user.id not in self.spoilers[msg_id]['deliveries']:
+                # reply with spoiler
+                try:
+                    await user.send("**Spoiler:** {}".format(self.spoilers[msg_id]['spoiler']))
+                    self.spoilers[msg_id]['deliveries'].append(user.id)
+                except (discord.Forbidden, discord.HTTPException) as err:
+                    # It's alright to fail silently if we are unable to DM the user
+                    pass
