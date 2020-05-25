c3b1367cda1943b2077c7b93f47fba30f5cc8061
aikaterna <20862007+aikaterna@users.noreply.github.com>
2020-05-25 11:02:51 -0700

    [Wat] Update + fix class
    Closes #113

--- wat/wat.py
+++ wat/wat.py
@@ -3,22 +3,19 @@ import re
 
 from redbot.core import checks, Config, commands
 
+PATTERN = re.compile(r"w+h*[aou]+t+[?!]*", re.IGNORECASE)
 
-class Wat(command.Cog):
+
+class Wat(commands.Cog):
 
     """Repeat messages when other users are having trouble hearing"""
 
-    default_global_settings = {
-        "channels_ignored": [],
-        "guilds_ignored": []
-    }
+    default_global_settings = {"channels_ignored": [], "guilds_ignored": []}
 
     def __init__(self, bot):
         self.bot = bot
         self.conf = Config.get_conf(self, identifier=527690525)
-        self.conf.register_global(
-            **self.default_global_settings
-        )
+        self.conf.register_global(**self.default_global_settings)
 
     @commands.group(name="watignore", pass_context=True, no_pm=True)
     @checks.admin_or_permissions(manage_guild=True)
@@ -35,12 +32,10 @@ class Wat(command.Cog):
         guilds = await self.conf.guilds_ignored()
         if guild.id in guilds:
             guilds.remove(guild.id)
-            await ctx.send("wot? Ok boss, I will no longer "
-                           "ignore this server.")
+            await ctx.send("wot? Ok boss, I will no longer ignore this server.")
         else:
             guilds.append(guild.id)
-            await ctx.send("what? Fine, I will ignore "
-                           "this server.")
+            await ctx.send("what? Fine, I will ignore this server.")
         await self.conf.guilds_ignored.set(guilds)
 
     @watignore.command(name="channel", pass_context=True, no_pm=True)
@@ -52,23 +47,18 @@ class Wat(command.Cog):
         chans = await self.conf.channels_ignored()
         if chan.id in chans:
             chans.remove(chan.id)
-            await ctx.send("wut? Ok, I will no longer "
-                           "ignore this channel.")
+            await ctx.send("wut? Ok, I will no longer ignore this channel.")
         else:
             chans.append(chan.id)
-            await ctx.send("wat? Alright, I will ignore "
-                           "this channel.")
+            await ctx.send("wat? Alright, I will ignore this channel.")
         await self.conf.channels_ignored.set(chans)
 
-    # Come up with a new method to ignore bot commands
     @commands.Cog.listener()
-    async def on_message(self, message):
+    async def on_message_without_command(self, message):
         if message.guild is None:
             return
         if message.author.bot:
             return
-        #if self.is_command(message):
-        #    return
         content = message.content.lower().split()
         if len(content) != 1:
             return
@@ -77,18 +67,17 @@ class Wat(command.Cog):
         if message.channel.id in await self.conf.channels_ignored():
             return
 
-        pattern = re.compile(r'w+h*[aou]+t+[?!]*', re.IGNORECASE)
-        if pattern.fullmatch(content[0]):
+        if PATTERN.fullmatch(content[0]):
             async for before in message.channel.history(limit=5, before=message):
                 author = before.author
                 name = author.display_name
                 content = before.clean_content
-                if not author.bot\
-                        and not author == message.author\
-                        and not pattern.fullmatch(content):
-                        #and not self.is_command(before)\
+                if (
+                    not author.bot
+                    and not author == message.author
+                    and not PATTERN.fullmatch(content)
+                ):
                     emoji = "\N{CHEERING MEGAPHONE}"
-                    msg = "{0} said, **{1}   {2}**".format(name, emoji,
-                                                           content)
+                    msg = f"{name} said, **{emoji}   {content}**"
                     await message.channel.send(msg)
                     break
