d9f42ac63ff3ff78876736df143038a8f515834c
aikaterna <20862007+aikaterna@users.noreply.github.com>
2020-08-26 09:19:59 -0700

    phrasing

--- reactpoll/__init__.py
+++ reactpoll/__init__.py
@@ -1,6 +1,6 @@
 from .reactpoll import ReactPoll
 
-__red_end_user_data_statement__ = "This cog does store Discord IDs as needed for operation temporary during a poll, which are automatically deleted when it ends."
+__red_end_user_data_statement__ = "This cog stores Discord IDs as needed for operation temporary during a poll which are automatically deleted when it ends."
 
 
 def setup(bot):
