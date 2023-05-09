38215b8a532b93f1b4fd05c43af529b4aa7c2ec3
aikaterna <20862007+aikaterna@users.noreply.github.com>
2023-05-08 20:26:13 -0700

    Async setup

--- reactpoll/__init__.py
+++ reactpoll/__init__.py
@@ -3,5 +3,5 @@ from .reactpoll import ReactPoll
 __red_end_user_data_statement__ = "This cog stores Discord IDs as needed for operation temporary during a poll which are automatically deleted when it ends."
 
 
-def setup(bot):
-    bot.add_cog(ReactPoll(bot))
+async def setup(bot):
+    await bot.add_cog(ReactPoll(bot))
--- smite/__init__.py
+++ smite/__init__.py
@@ -3,5 +3,5 @@ from .smite import Smite
 __red_end_user_data_statement__ = "This cog stores discord IDs as needed for operation."
 
 
-def setup(bot):
-    bot.add_cog(Smite(bot))
+async def setup(bot):
+    await bot.add_cog(Smite(bot))
